e5e0c7fb243ae9d4340951726a0ecc579ac9f0ba
Domininkas <domas.krabas@gmail.com>
2022-11-27 21:22:48 +0200

    Added auto-migration

--- Program.cs
+++ Program.cs
@@ -34,6 +34,14 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var db = services.GetRequiredService<IdentityContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
