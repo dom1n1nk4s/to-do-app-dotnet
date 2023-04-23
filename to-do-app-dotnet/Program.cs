ed2443f70b39637a0f6c5069867eb487143ba74a
dom1n1nk4s <36505835+dom1n1nk4s@users.noreply.github.com>
2023-04-23 11:04:31 +0000

    Fixed service not being DI'ed

--- to-do-app-dotnet/Program.cs
+++ to-do-app-dotnet/Program.cs
@@ -36,7 +36,7 @@ internal class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-
+        builder.Services.AddScoped<IIdentityContext,IdentityContext>();
         var app = builder.Build();
 
         using (var scope = app.Services.CreateScope())
